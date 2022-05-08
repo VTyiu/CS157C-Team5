76849c2056141ecabc13741019454305780877d0
aaronli-git <li.aaron36@gmail.com>
2022-05-07 19:45:40 -0700

    Tempory changes to User. Fixed the root in index.js

--- server/index.js
+++ server/index.js
@@ -8,60 +8,94 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://user1:valstats@cluster0.vn2kb.mongodb.net/valstats?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://user1:valstats@cluster0.vn2kb.mongodb.net/valstats?retryWrites=true&w=majority"
+);
+
+// app.get("/getUsers", (req, res) => {
+//     UserModel.find({}, (err, result) => {
+//         if (err) {
+//             res.json(err);
+//         } else {
+//             res.json(result); // will send back result that we got from the table
+//         }
+//     });
+// });
 
 // req: request - get info from front end, res: response - send info from front end to back end
-app.get("/getUsers", (req, res) => {
-    UserModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err); // send error back to front end
-        } else {
-            res.json(result); // will send back result that we got from the table
-        }
-    });
+app.post("/loginUser", async (req, res) => {
+  const user = await UserModel.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  if (user) {
+    res.send("Logged int");
+    console.log(res.json(user));
+    return res.json({ status: "ok", user: true });
+  } else {
+    return res.json({ status: "error", user: false }); // send error back to front end
+  }
 });
 
 app.post("/createUser", async (req, res) => {
-    const user = req.body; // user represents data we wanna send to database
-    const newUser = new UserModel(user);
-    await newUser.save();
-
+  try {
+    const user = await UserModel.create({
+      // user represents data we wanna send to database
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      rank: req.body.rank,
+    });
     res.json(user); // won't be using this data but will send it back so we know that we got back the correct info in backend
+  } catch (err) {
+    res.json({ status: "error", error: "Duplicate email" });
+  }
 });
 
 app.get("/getMatches", (req, res) => {
-    MatchModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    });
+  MatchModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
 });
 
 app.post("/createMatch", async (req, res) => {
-    const match = req.body;
-    const newMatch = new MatchModel(match);
-    await newMatch.save();
-    res.json(match);
+  const match = req.body;
+  const newMatch = new MatchModel(match);
+  await newMatch.save();
+  res.json(match);
 });
 
-app.put("/update", async (req, res) => { // .put is used to update stuff
-    const newMapName = req.body.newMapName;
-    const _id = req.body._id;
-    try {
-        await MatchModel.findById(_id, (error, matchToUpdate) => {
-            matchToUpdate.mapName = newMapName;
-            matchToUpdate.save();
+app.put("/update", async (req, res) => {
+  // .put is used to update stuff
+  const newMapName = req.body.newMapName;
+  const _id = req.body._id;
+  try {
+    await MatchModel.findById(_id, (error, matchToUpdate) => {
+      matchToUpdate.mapName = newMapName;
+      matchToUpdate.save();
+    });
+  } catch (err) {
+    console.log(err);
+  }
 
-        });
-    } catch(err){
-        console.log(err);
-    }
-
-    res.send("updated");
-}) 
+  res.send("updated");
+});
 
 app.listen(3001, () => {
-    console.log("SERVER RUNS PERFECTLY!");
+  console.log("SERVER RUNS PERFECTLY!");
 });
--- server/models/Users.js
+++ server/models/Users.js
@@ -17,42 +17,53 @@ const mongoose = require("mongoose");
 // });
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    rank: {
-        type: String,
-        required: true
-    },
-    //match: [matchSchema],
-    matches: [
-        {
-            match_id: {
-                type: Number,
-                required: true
-            },
-            map: {
-                type: String,
-                required: true
-            },
-            agent: {
-                type: String,
-                required: true
-            },
-            gun: {
-                type: String,
-                required: true
-            }
-        }
-    ],
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  rank: {
+    type: String,
+    required: true,
+  },
+  //match: [matchSchema],
+  //   matches: [
+  //     {
+  //       match_id: {
+  //         type: Number,
+  //         required: true,
+  //       },
+  //       map: {
+  //         type: String,
+  //         required: true,
+  //       },
+  //       agent: {
+  //         type: String,
+  //         required: true,
+  //       },
+  //       gun: {
+  //         type: String,
+  //         required: true,
+  //       },
+  //     },
+  //   ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model("users", UserSchema);
+const model = mongoose.model("users", UserSchema);
+
+module.exports = model;
 
 // const UserModel = mongoose.model("users", UserSchema);
 // module.exports = UserModel;
