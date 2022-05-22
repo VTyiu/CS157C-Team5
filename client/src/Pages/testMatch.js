f59874f95be5f88a947d62471cfda87127062dfb
vtang <78068910+VTyiu@users.noreply.github.com>
2022-05-22 00:19:32 -0700

    added delete, working on update button

--- client/src/Pages/testMatch.js
+++ client/src/Pages/testMatch.js
@@ -14,7 +14,7 @@ const Profile = () => {
   useEffect(() => {
     Axios.get("http://localhost:3001/getMatches")
       .then((response) => {
-        const currUserMatches = _.filter(response.data, {'user_id': 0});
+        const currUserMatches = _.filter(response.data, { 'user_id': 0 });
         console.log("current user matches", currUserMatches);
         setListOfMatches(currUserMatches);
       })
@@ -23,28 +23,37 @@ const Profile = () => {
       });
   }, []);
 
+  const deleteMatch = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then(() => {
+      setListOfMatches(listOfMatches.filter((val) => {
+        return val._id != id;
+      }))
+    });
+  };
+
   return (
     <div>
-        <div className="profile-container">
-          <div className="profile-page-background">
-            <div className="profile-page-container">
-              <h1>Profile Page</h1>
-              <button
-                className="openMatchForm"
-                onClick={() => {
-                  setOpenMatchForm(true);
-                }}
-              >
-                Add Match
-              </button>
-            </div>
+      <div className="profile-container">
+        <div className="profile-page-background">
+          <div className="profile-page-container">
+            <h1>Profile Page</h1>
+            <button
+              className="openMatchForm"
+              onClick={() => {
+                setOpenMatchForm(true);
+              }}
+            >
+              Add Match
+            </button>
           </div>
-          <div className="profile-matches-background">
-            <div className="profile-matches-container">
-              <h1> Recent Matches </h1>
-              {listOfMatches.map((val) => {
+        </div>
+        <div className="profile-matches-background">
+          <div className="profile-matches-container">
+            <h1> Recent Matches </h1>
+            {listOfMatches.map((val) => {
               return (
-                  <div className="match">
+                <div className="match_container">
+                  <div className="match_info">
                     <h3>Agent: {val.agent}</h3>
                     <h3>Map name: {val.mapName}</h3>
                     <h3>Number of kills: {val.kills}</h3>
@@ -52,16 +61,28 @@ const Profile = () => {
                     <h3>Main gun used: {val.gun}</h3>
                     <h3>Comments: {val.comments}</h3>
                   </div>
+                  
+                  <div className="match_button">
+
+                    <button onClick={() => {
+                    }}>Update</button>
+
+                    <button onClick={() => {
+                      deleteMatch(val._id);
+                    }}>Delete</button>
+
+                  </div>
+                </div>
               );
             })}
-            </div>
-          <div className="profile-match-container">
+          </div>
+          {/* <div className="profile-match-container">
             
 
-          </div>
-          </div>
-          {openMatchForm && <MatchForm closeMatchForm={setOpenMatchForm} />}
+          </div> */}
         </div>
+        {openMatchForm && <MatchForm closeMatchForm={setOpenMatchForm} />}
+      </div>
     </div>
   );
 };
